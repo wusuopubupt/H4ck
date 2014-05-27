cc90017f6ea66d8e90c2e2726ff60eafc0870600
d4shman <wdxbupt2009@gmail.com>
2014-05-27 13:57:36 +0800

    Update popup.js

--- cookieModifier/popup.js
+++ cookieModifier/popup.js
@@ -4,9 +4,11 @@
  * @author  :  d4sh wang
  * @mail    :  d4shman@gmail.com
  * @version :  0.9
+ * @date	:  2013-05-16
  *
  */
- 
+var url = "";
+
 // trim the spaces
 String.prototype.trim = function() {
 	var re = /(^\s*)|(\s*)$/g;
@@ -18,42 +20,10 @@ function $(s) {
 	return document.getElementById(s);
 }
 
-//get current selected url
-//reference : http://stackoverflow.com/questions/1979583/how-can-i-get-the-url-for-a-google-chrome-tab
-var url;
-chrome.tabs.getSelected(null,function(tab) {
-    url = tab.url;
-});
-
 // set cookie
 function set_cookie(cookie){
 	fragment = url.split('/');
-	/*eg:
-	 *var url = http://stackoverflow.com/questions/1034621.html
-	 *var arr = url.split('/')
-	 *console.log(arr)
-	 *["http:", "", "stackoverflow.com", "questions", "1034621.html"]
-	 */
-	
 	url = fragment[0] + '//' + fragment[2];
-	
-	/*
-	ck = cookie.split(';');
-	for(i in ck){
-		c = ck[i].trim();
-		if(!c) continue;
-		k = c.split('=')[0].trim();
-		v = c.split('=')[1].trim();
-		
-		chrome.cookies.set({
-			'url': url,
-			'name': k,
-			'value': v,
-		});
-	}*/
-	
-	name   = cookie.split('=')[0].trim();
-	value = cookie.split('=')[1].trim();
 	
 	// init chrome.cookies 
 	// reference : http://src.chromium.org/viewvc/chrome/trunk/src/chrome/common/extensions/docs/examples/api/cookies/manager.js
@@ -61,26 +31,51 @@ function set_cookie(cookie){
 		chrome.cookies = chrome.experimental.cookies;
 	}
 	
-	alert(url);
-	alert(name);
-	alert(value);
-	
-	//reference : https://developer.chrome.com/extensions/cookies#method-set
-	chrome.cookies.set({
-		'url' : url,
-		'name' : name,
-		'value': value
-	}, function() {
-		alert("succeed!" + document.cookie);
+	//alert("set cookie:" + cookie);
+	ck = cookie.split(';');
+	for(i in ck){
+		c = ck[i].trim();
+		if(!c) continue;
+		k = c.split('=')[0].trim();
+		v = c.split('=')[1].trim();
+		
+		//reference : https://developer.chrome.com/extensions/cookies#method-set
+		chrome.cookies.set({
+			'url': url,
+			'name': k,
+			'value': v,
+		});
+	}
+}
+
+// get cookie
+function get_cookie() {
+	var cookie_str = "";
+	chrome.cookies.getAll({'url':url}, function(cookie) {
+		for(i in cookie) {
+			name = cookie[i].name;
+			value = cookie[i].value;
+			cookie_str += (name + "=" + value + ";\n");
+		}
+		//alert(cookie_str);
+		$('cookie').value = cookie_str;
 	})
 }
 
 function onload() {
+	get_cookie();
+	
 	$('submit').addEventListener('click', function () {
 		set_cookie($('cookie').value);
 	});
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-	onload();
+	if(url == "") {
+		chrome.tabs.getSelected(null,function(tab) {
+			url = tab.url;
+			//alert("DOMContentLoaded url: " + url); 异步事件，把onload函数放在回调函数里，url才能取得到
+			onload();
+		});
+	}
 });

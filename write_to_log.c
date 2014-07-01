a5977ecef3c30dc5c27cde79d7df33455aa9b82e
d4shman <wdxbupt2009@gmail.com>
2014-07-01 11:30:03 +0800

    Update write_to_log.c

--- write_to_log.c
+++ write_to_log.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include <time.h>
 
-int main() {
-    char buff[20];
+void write_log(char *log_str) {
+    FILE *file;
+    file = fopen("/var/log/voyage/gearman_c_worker.log","a+");
+
+    char time_buff[20];
     struct tm *sTm;
-
-    time_t now = time(0);
+    time_t now  = time(0);
     sTm = gmtime(&now);
+    strftime(time_buff, sizeof(time_buff), "%Y-%m-%d %H:%M:%S", sTm);
 
-    strftime(buff, sizeof(buff), "%Y-%m-%d %H:%M:%S", sTm);
-    printf("%s %s\n", buff, "hello, world");
 
-    return 0;
+    fprintf(file,"%s %s\n",time_buff, log_str); /*writes*/
+    fclose(file);
 }
 
+int main() {
+    char *log = "hello, world!";
+
+    write_log(log);
+    return 0;
+}
